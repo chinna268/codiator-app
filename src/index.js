0009e870a43018464382c0a3e1ac04223152ac73
chinna2688 <keshavreddy8899@gmail.com>
2021-12-16 09:29:40 +0530

    changed project name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom";
+
+//  importing of bootsrap it should be always on the top 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//importing of bootstrap for js functionality
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+import './assests/CSS/style.css';
+// custorm css link
+ 
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+// ReactDom.render(Data,Location);
+ReactDom.render(<App/>,
+        document.getElementById("root"));
